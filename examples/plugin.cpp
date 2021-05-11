31f63257957eb6b073e9a8e066b8498b18cc573b
Samuel Grayson <sam@samgrayson.me>
2021-05-11 11:56:55 -0500

    wip (works with gtsam)

--- examples/plugin.cpp
+++ examples/plugin.cpp
@@ -72,14 +72,13 @@ public:
     void feed_imu_cam(switchboard::ptr<const imu_cam_type> datum) {
         // Ensures that slam doesnt start before valid IMU readings come in
         if (datum == nullptr) {
-            assert(previous_timestamp == 0);
             return;
         }
 
         // This ensures that every data point is coming in chronological order If youre failing this assert, 
         // make sure that your data folder matches the name in offline_imu_cam/plugin.cc
-        assert(datum->dataset_time > previous_timestamp);
-        previous_timestamp = datum->dataset_time;
+        assert(datum->time > previous_timestamp);
+        previous_timestamp = datum->time;
 
         // Store the datum back to our buffer
         imu_cam_buffer = datum;
@@ -89,7 +88,7 @@ public:
 
         // Feed the IMU measurement. There should always be IMU data in each call to feed_imu_cam
         assert((datum->img0.has_value() && datum->img1.has_value()) || (!datum->img0.has_value() && !datum->img1.has_value()));
-        kimera_pipeline.fillSingleImuQueue(VIO::ImuMeasurement(datum->dataset_time, imu_raw_vals));
+        kimera_pipeline.fillSingleImuQueue(VIO::ImuMeasurement(datum->time.time_since_epoch().count(), imu_raw_vals));
 
         // If there is not cam data this func call, break early
         if (!datum->img0.has_value() && !datum->img1.has_value()) {
@@ -113,10 +112,10 @@ public:
         VIO::CameraParams left_cam_info = kimera_pipeline_params.camera_params_.at(0);
         VIO::CameraParams right_cam_info = kimera_pipeline_params.camera_params_.at(1);
         kimera_pipeline.fillLeftFrameQueue(VIO::make_unique<VIO::Frame>(kimera_current_frame_id,
-                                                                    datum->dataset_time,
+																	datum->time.time_since_epoch().count(),
                                                                     left_cam_info, img0));
         kimera_pipeline.fillRightFrameQueue(VIO::make_unique<VIO::Frame>(kimera_current_frame_id,
-                                                                    datum->dataset_time,
+																	datum->time.time_since_epoch().count(),
                                                                     right_cam_info, img1));
 
 		// Ok this needs to be filed in a later task to dig up what sets errno in Kimera
@@ -167,8 +166,8 @@ public:
         }));
 
         _m_imu_integrator_input.put(_m_imu_integrator_input.allocate<imu_integrator_input>(imu_integrator_input{
-			double(imu_cam_buffer->dataset_time) * (1/1000) * (1/1000) * (1/1000),
-			-0.05,
+			imu_cam_buffer->time,
+			duration(std::chrono::milliseconds{-50}),
             imu_params{
                 kimera_pipeline_params.imu_params_.gyro_noise_,
                 kimera_pipeline_params.imu_params_.acc_noise_,
@@ -199,7 +198,7 @@ private:
     
     //const imu_cam_type* imu_cam_buffer;
     switchboard::ptr<const imu_cam_type> imu_cam_buffer;
-    double previous_timestamp = 0.0;
+    time_point previous_timestamp = time_point{duration{-1}};
 };
 
 PLUGIN_MAIN(kimera_vio)
